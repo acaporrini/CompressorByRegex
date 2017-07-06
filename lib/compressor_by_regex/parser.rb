edd20bb29c6d2b533c5173dce0e61a44e4342981
acaporrini <alessan.drocaporrini@ip-172-16-12-8.eu-west-1.compute.internal>
2017-07-06 14:07:16 +0200

    Adding support for wildcards parsing in postalcodes

--- lib/compressor_by_regex/parser.rb
+++ lib/compressor_by_regex/parser.rb
@@ -5,15 +5,60 @@ module CompressorByRegex
     attr_reader :postal_codes
 
     def initialize(postal_codes)
-      @postal_codes = postal_codes
+      @postal_codes = postal_codes.dup
     end
 
     def parse
+      wildcard_postalcodes = extract_wildcard_postalcodes
+      wildcard_postalcodes_parsed = parse_wildcard_postalcodes(wildcard_postalcodes)
+
       postal_codes_ranges = group_by_ranges
-      create_regex_based_on_ranges
+      postal_codes_parsed = create_regex_based_on_ranges
+
+      wildcard_postalcodes_parsed.any? ? postal_codes_parsed.concat(wildcard_postalcodes_parsed) : postal_codes_parsed
     end
 
     private
+      def extract_wildcard_postalcodes
+        @postal_codes = @postal_codes.partition {|postal_code| postal_code.include?("%")}
+        wildcard_postalcodes = @postal_codes.first
+        @postal_codes = @postal_codes.last
+        wildcard_postalcodes
+      end
+
+      def parse_wildcard_postalcodes(wildcard_postalcodes)
+        wildcard_postalcodes.map do |postal_code|
+        result =
+          if postal_code.start_with?("LIKE")
+            postal_code.slice!("LIKE ")
+            parse_positive_wildcard(postal_code)
+          elsif postal_code.start_with?("NOT LIKE")
+            postal_code.slice!("NOT LIKE ")
+            parse_negative_wildcard(postal_code)
+          elsif postal_code.include?("%")
+            parse_positive_wildcard(postal_code)
+          else
+            nil
+          end
+
+          result
+        end
+      end
+
+      def parse_positive_wildcard(postal_code)
+        "/^#{postal_code.gsub("%", ".*")}$/"
+      end
+
+      def parse_negative_wildcard(postal_code)
+        if postal_code.start_with? "%"
+          postal_code.slice!("%")
+          "/^.*(?<!#{postal_code})$/"
+        elsif postal_code.end_with? "%"
+          postal_code.slice!("%")
+          "/^(?!#{postal_code}).*$/"
+        end
+      end
+
       def group_by_ranges
         _postal_codes = @postal_codes.sort
         actual = _postal_codes.first
--- test/parser_test.rb
+++ test/parser_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 
 class CompressorByRegex::ParserTest < Minitest::Test
   def test_parse
-    parser = CompressorByRegex::Parser.new(["0010","0012", "0022", "1005", "0023", "0024", "0027","1001", "0013", "1002"])
-    assert_equal(["/^0010$/", "/^001[23]$/", "/^002[2-4]$/", "/^0027$/", "/^100[12]$/", "/^1005$/"], parser.parse)
+    parser = CompressorByRegex::Parser.new(["LIKE 100%", "LIKE %100", "NOT LIKE 200%", "NOT LIKE %200", "%300", "300%", "0010","0012", "0022", "1005", "0023", "0024", "0027","1001", "0013", "1002"])
+    assert_equal(["/^0010$/", "/^001[23]$/", "/^002[2-4]$/", "/^0027$/", "/^100[12]$/", "/^1005$/", "/^100.*$/", "/^.*100$/", "/^(?!200).*$/", "/^.*(?<!200)$/", "/^.*300$/", "/^300.*$/"], parser.parse)
   end
 end
